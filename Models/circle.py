c1ed6b4b737e15a7549c5de8b140b6f414db5200
Slav Kirilov <slav.kirilov@coosto.com>
2021-06-14 09:35:53 +0300

    Updated the face generation

--- Models/circle.py
+++ Models/circle.py
@@ -4,40 +4,36 @@ import math
 import mathutils
 import csv
 
-# Make a new BMesh
-csv_file = open('C:\\Users\\Slav Kirilov\\Desktop\\BlenderScripts\\Submarine.csv', newline='')
+csv_file = open('C:\\Users\\Slav Kirilov\\Desktop\\BlenderScripts\\Submarine\\Models\\Submarine.csv', newline='')
 submarine_data = csv.reader(csv_file, delimiter=',')
 
 for row in submarine_data:
-    bm = bmesh.new()
-    bmesh.ops.create_circle(
-        bm,
-        cap_ends=False,
-        radius=float(row[1]),
-        segments=30)
-            
-    bmesh.ops.translate(
-        bm,
-        verts=bm.verts,
-        vec=(0.0, 0.0, float(row[0])))
+   bm = bmesh.new()
+   bmesh.ops.create_circle(
+       bm,
+       cap_ends=False,
+       radius=float(row[1]),
+       segments=30)
+           
+   bmesh.ops.translate(
+       bm,
+       verts=bm.verts,
+       vec=(0.0, 0.0, float(row[0])))
 
-    me = bpy.data.meshes.new("Mesh")
-    bm.to_mesh(me)
-    bm.free()
+   me = bpy.data.meshes.new("Mesh")
+   bm.to_mesh(me)
+   bm.free()
 
 
-    # Add the mesh to the scene
-    obj = bpy.data.objects.new("Circle", me)
-    bpy.context.collection.objects.link(obj)
+   # Add the mesh to the scene
+   obj = bpy.data.objects.new("Circle", me)
+   bpy.context.collection.objects.link(obj)
 
 MSH_OBJS = [m for m in bpy.context.scene.objects if m.type == 'MESH']
 for OBJS in MSH_OBJS:
-    #Select all mesh objects
-    OBJS.select_set(state=True)
-
-    #Makes one active
-    bpy.context.view_layer.objects.active = OBJS
-    
+   OBJS.select_set(state=True)
+   bpy.context.view_layer.objects.active = OBJS
+   
 bpy.ops.object.join()
 
 bpy.ops.object.mode_set(mode='EDIT')
@@ -50,9 +46,16 @@ verts = bm.verts
 
 verts = sorted(verts, key= lambda vert: vert.co[2])
 
-for i in range(int(len(verts)-31)):
-    selected = [verts[i], verts[i+1], verts[i+30], verts[i+1+30]]
-    bmesh.ops.contextual_create(bm, geom=selected)
+for j in range(int(len(verts)/30)):
+    for i in range(j*30, (j+1)*30):
+        if i < (j+1)*30 - 1:
+            selected = [verts[i], verts[i+1], verts[i+30], verts[i+31]]
+            bmesh.ops.contextual_create(bm, geom=selected)
+        else:
+            selected = [verts[i], verts[j*30], verts[i + 30], verts[(j + 1)*30]]
+            bmesh.ops.contextual_create(bm, geom=selected)
+            break
+        
     
 
 bmesh.update_edit_mesh(me)
